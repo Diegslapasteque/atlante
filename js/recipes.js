1d40ce8597886eebb04929ff80d7987266d4ab5b
Nathan <nathan.mousset@etu.unilim.fr>
2020-02-02 11:22:43 +0100

    Runes

--- js/recipes.js
+++ js/recipes.js
@@ -34,39 +34,30 @@ var RECIPES = {
         new Resources('plumeDePhoenix', 'potion'),
     ],
     PARCHEMINDESOMEIL: [
-        new Resources('parchemin', 'scroll'),
         new Resources('incantationDeSomeil', 'scroll'),
     ],
     PARCHEMINAMOUR: [
-        new Resources('parchemin', 'scroll'),
         new Resources('incantationAmour', 'scroll'),
     ],
     PARCHEMINDEPERSUASION: [
-        new Resources('parchemin', 'scroll'),
         new Resources('incantationDePersuasion', 'scroll'),
     ],
     PARCHEMINOUBLIE: [
-        new Resources('parchemin', 'scroll'),
         new Resources('incantationOublie', 'scroll'),
     ],
     PARCHEMINDECOMPETENCE: [
-        new Resources('parchemin', 'scroll'),
         new Resources('incantationDeCompetance', 'scroll'),
     ],
     RUNEAFFUTAGE: [
         new Resources('pierre', 'rune'),
-        new Resources('glypheDeReparation', 'rune'),
     ],
     RUNEDEREPARATION: [
         new Resources('ferrite', 'rune'),
-        new Resources('glypheDeReparation', 'rune'),
     ],
     RUNEDEREPARATIONDEBIJOUX: [
         new Resources('lingotOr', 'rune'),
-        new Resources('glypheDeReparation', 'rune'),
     ],
     RUNEDEPROTECTIONDIVINE: [
         new Resources('lingotOr', 'rune'),
-        new Resources('glypheDivine', 'rune'),
     ],
 };
